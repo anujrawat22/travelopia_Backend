84a79677374185213922bb0eea3e1e8967f380f8
anujrawat22 <anuj22rawat20@gmail.com>
2023-04-06 01:10:33 +0530

    cors added

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const { connection } = require('./config/db')
-const { dataRouter } = require('./routes/data.routes')
+const express = require("express");
+const { connection } = require("./config/db");
+const { dataRouter } = require("./routes/data.routes");
 
+const cors = require("cors");
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(cors());
+app.use("/data", dataRouter);
 
-app.use(express.json())
-
-app.use("/data", dataRouter)
-
-
-app.listen(process.env.PORT||8080, async ()=>{
-    try{
-      await connection;
-      console.log("Connected to DB")
-      console.log(`Listening on PORT ${process.env.PORT}`)
-    }catch(err){
-        console.log(err)
-    }
-})
+app.listen(process.env.PORT || 8080, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
+    console.log(`Listening on PORT ${process.env.PORT}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
